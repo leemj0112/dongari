95e5e23259b8b84040231c9197fb97e8f57dadde
leemj0112 <163072105+leemj0112@users.noreply.github.com>
2024-05-04 17:20:18 +0900

    점프 기능 구현
    맵 일부 삭제

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,25 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
     public float MaxSpeed;
+    public float JumpPower;
+
+    bool IsJumping;
+
     Rigidbody2D PlayerRigid;
 
     private void Awake()
     {
-         PlayerRigid = GetComponent<Rigidbody2D>();
-    }
-
-    private void Update()
-    {
-        //속도 멈춤
-        if (Input.GetButtonUp("Horizontal"))
-        {
-            PlayerRigid.velocity = new Vector2(PlayerRigid.velocity.normalized.x * 0.5f, PlayerRigid.velocity.y);
-        }
+        PlayerRigid = GetComponent<Rigidbody2D>();
+        IsJumping = false;
     }
 
     private void FixedUpdate()
@@ -30,11 +23,34 @@ public class PlayerMove : MonoBehaviour
 
 
         //오른쪽 최고 속도
-        if(PlayerRigid.velocity.x > MaxSpeed )
+        if (PlayerRigid.velocity.x > MaxSpeed)
             PlayerRigid.velocity = new Vector2(MaxSpeed, PlayerRigid.velocity.y);
         //왼쪽 최고 속도
         else if (PlayerRigid.velocity.x < MaxSpeed * (-1))
-            PlayerRigid.velocity = new Vector2(MaxSpeed*(-1), PlayerRigid.velocity.y);
+            PlayerRigid.velocity = new Vector2(MaxSpeed * (-1), PlayerRigid.velocity.y);
+    }
+    private void Update()
+    {
+        //점프
+        if (Input.GetButtonDown("Jump") && !IsJumping)
+        {
+            PlayerRigid.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
+            IsJumping = true;
+        }
 
+        //속도 멈춤
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            PlayerRigid.velocity = new Vector2(PlayerRigid.velocity.normalized.x * 0.5f, PlayerRigid.velocity.y);
+        }
+    }
+
+    //충돌 감지 및 점프 초기화
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            IsJumping = false;
+        }
     }
 }
